22243276048628c288fac45e933e610f91795ae8
Gunjan9090 <gg2354875@gmail.com>
2021-06-30 14:25:13 +0530

    Commit message

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,22 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('client_name');
+            $table->string('trn_details');
+            $table->string('address');
+            $table->integer('mobile')->unique();
+            $table->string('whatsaap_no');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('emirate');
+            $table->string('country');
+            $table->string('trade_license');
+            $table->string('vat_period');
+            $table->string('vat_percentage');
+            $table->string('fta_login');
+            $table->string('username');
             $table->string('password');
+            $table->timestamp('email_verified_at')->nullable();
+            $table->integer('label')->unique()->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>PN-Education | Registration</title>
+  <title>VATZAPP| Registration</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -26,9 +26,9 @@
       <p class="login-box-msg">Register a new membership</p>
       <form method="POST" action="{{ route('register') }}">
         @csrf
-        <div class="input-group mb-3">
-           <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-                @error('name')
+        <div class="input-group mb-3"><!--client name-->
+           <input id="client_name" type="text" placeholder="client name" class="form-control @error('client_name') is-invalid @enderror" name="client_name" value="{{ old('client_name') }}" required autocomplete="client_name" autofocus>
+                @error('client_name')
                 <span class="invalid-feedback" role="alert">
                 <strong>{{ $message }}</strong>
                 </span>
@@ -38,9 +38,61 @@
               <span class="fas fa-user"></span>
             </div>
           </div>
-        </div>
-        <div class="input-group mb-3">
-          <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+        </div><!--client name-->
+        <div class="input-group mb-3"><!--trn details-->
+           <input id="trn_details" type="text" placeholder="trn details" class="form-control @error('trn_details') is-invalid @enderror" name="trn_details" value="{{ old('trn_details') }}" required autocomplete="trn_details" autofocus>
+                @error('trn_details')
+                <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+                </span>
+                @enderror
+            <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-user"></span>
+            </div>
+          </div>
+        </div><!--trn details-->
+        <div class="input-group mb-3"><!--address-->
+           <input id="address" type="address" placeholder=" Address" class="form-control @error('address') is-invalid @enderror" name="address" value="{{ old('address') }}" required autocomplete="address" autofocus>
+                @error('address')
+                <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+                </span>
+                @enderror
+            <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-user"></span>
+            </div>
+          </div>
+        </div><!--address-->
+        <div class="input-group mb-3"><!--Mobile number-->
+           <input id="mobile" type="mobile" placeholder="Mobile " class="form-control @error('mobile') is-invalid @enderror" name="mobile" value="{{ old('mobile') }}" required autocomplete="mobile" autofocus>
+                @error('mobile')
+                <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+                </span>
+                @enderror
+            <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-user"></span>
+            </div>
+          </div>
+        </div><!--Mobile Number-->
+        <div class="input-group mb-3"><!--whatsaap number-->
+           <input id="whatsaap_no" type="mobile" placeholder="whatsaap Number" class="form-control @error('whatsaap_no') is-invalid @enderror" name="whatsaap_no" value="{{ old('whatsaap_no') }}" required autocomplete="whatsaap_no" autofocus>
+               <!--  @error('whatsaap_no')
+                <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+                </span>
+                @enderror -->
+            <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-user"></span>
+            </div>
+          </div>
+        </div><!--whatsaap Number-->
+        <div class="input-group mb-3"><!--email-->
+          <input id="email" type="email" placeholder="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
                 @error('email')
                 <span class="invalid-feedback" role="alert">
                 <strong>{{ $message }}</strong>
@@ -51,10 +103,102 @@
               <span class="fas fa-envelope"></span>
             </div>
           </div>
-        </div>
+        </div><!--email-->
+        <div class="input-group mb-3"><!--emirate-->
+          <input id="emirate" type="text" placeholder="emirate" class="form-control @error('emirate') is-invalid @enderror" name="emirate" value="{{ old('emirate') }}" required autocomplete="emirate">
+                @error('emirate')
+                <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+                </span>
+                @enderror
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-envelope"></span>
+            </div>
+          </div>
+        </div><!--emirate-->
+         <div class="input-group mb-3"><!--country-->
+           <input id="country" type="text" placeholder=" Country" class="form-control @error('country') is-invalid @enderror" name="country" value="{{ old('country') }}" required autocomplete="country" autofocus>
+                @error('country')
+                <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+                </span>
+                @enderror
+            <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-user"></span>
+            </div>
+          </div>
+        </div><!--country-->
+         <div class="input-group mb-3"><!--Trade Liscense-->
+           <input id="trade_license" type="text" placeholder=" trade_license" class="form-control @error('trade_license') is-invalid @enderror" name="trade_license" value="{{ old('trade_license') }}" required autocomplete="trade_license" autofocus>
+                @error('name')
+                <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+                </span>
+                @enderror
+            <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-user"></span>
+            </div>
+          </div>
+        </div><!--Trade License-->
+         <div class="input-group mb-3"><!--Vat Period-->
+           <input id="vat_period" type="text" placeholder=" Vat Period" class="form-control @error('vat_period') is-invalid @enderror" name="vat_period" value="{{ old('vat_period') }}" required autocomplete="vat_period" autofocus>
+                @error('vat_period')
+                <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+                </span>
+                @enderror
+            <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-user"></span>
+            </div>
+          </div>
+        </div><!--vat period-->
+         <div class="input-group mb-3"><!--Vat percentage-->
+           <input id="vat_percentage" type="text" placeholder="Vat percentage" class="form-control @error('vat_percentage') is-invalid @enderror" name="vat_percentage" value="{{ old('vat_percentage') }}" required autocomplete="vat_percentage" autofocus>
+                @error('vat_percentage')
+                <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+                </span>
+                @enderror
+            <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-user"></span>
+            </div>
+          </div>
+        </div><!--vat percentage-->
+        <div class="input-group mb-3"><!--FTA login-->
+           <input id="fta_login" type="text" placeholder="fta login" class="form-control @error('fta_login') is-invalid @enderror" name="fta_login" value="{{ old('fta_login') }}" required autocomplete="fta_login" autofocus>
+                @error('fta_login')
+                <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+                </span>
+                @enderror
+            <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-user"></span>
+            </div>
+          </div>
+        </div><!--FTA login-->
+        <div class="input-group mb-3"><!--username-->
+           <input id="username" type="text" placeholder="username" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}" required autocomplete="username" autofocus>
+                @error('username')
+                <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+                </span>
+                @enderror
+            <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-user"></span>
+            </div>
+          </div>
+        </div><!--Username-->
 
-        <div class="input-group mb-3">
-            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+
+        <div class="input-group mb-3"><!--password-->
+            <input id="password" type="password" placeholder="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
                 @error('password')
                 <span class="invalid-feedback" role="alert">
                 <strong>{{ $message }}</strong>
@@ -66,33 +210,29 @@
               <span class="fas fa-lock"></span>
             </div>
           </div>
-        </div>
-       
-        <div class="input-group mb-3">
-          <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+        </div><!--password-->
+        <div class="input-group mb-3"><!--password confrm-->
+          <input id="password-confirm" placeholder="Confirm Password" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
           
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-lock"></span>
             </div>
           </div>
-        </div>
-
-        <div class="input-group mb-3">
+        </div><!--password confrm-->
+        <div class="input-group mb-3"><!--label-->
            <select name="label" class="form-control  @error('label') is-invalid @enderror">
             <option value="Null">Select Label</option>
             <option value="0">Checker</option>
             <option value="1">validator</option>
             <option value="2">Admin</option>
             <option value="3">Super Admin</option>
-            
-
-           </select>
-                 @error('label')
-                  <span class="invalid-feedback" role="alert">
-                  <strong>{{ $message }}</strong>
-                  </span>
-                  @enderror
+          </select>
+              @error('label')
+                <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+                </span>
+                @enderror
             <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-user"></span>
@@ -111,12 +251,11 @@
           </div>
           <!-- /.col -->
          <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
+          <div class="col-md-6 offset-md-4">
+            <button type="submit" class="btn btn-primary">
+            {{ __('Register') }}</button>
+          </div>
+        </div>
           <!-- /.col -->
         </div>
       </form>
